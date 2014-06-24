4c61a1edba431f84d04b0cba55081ba718757e68
Juan Luis Boya García <ntrrgc@gmail.com>
2014-06-25 00:19:35 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ENTITIES=(.vim .vimrc)
+ENTITIES=(.vim .vimrc .gitignore_global .gitconfig)
 for entity in ${ENTITIES[@]}; do
   ln -s "$DIR/$entity" "$HOME/$entity"
 done
