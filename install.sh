08515704cf1fbcea13597e124913a96d08b82acb
Juan Luis Boya García <ntrrgc@gmail.com>
2014-06-25 00:50:58 +0200

    Check existence

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ENTITIES=(.vim .vimrc .gitignore_global .gitconfig)
 for entity in ${ENTITIES[@]}; do
-  ln -s "$DIR/$entity" "$HOME/$entity"
+  if [ ! -L "$HOME/$entity" ]; then
+    ln -s "$DIR/$entity" "$HOME/$entity"
+  fi
 done
