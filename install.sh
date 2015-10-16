5ee35873260cad8b301db65b76c13e6892dcd6b0
Juan Luis Boya García <ntrrgc@gmail.com>
2015-10-16 15:33:52 +0200

    Use relative symlinks

--- install.sh
+++ install.sh
@@ -6,20 +6,25 @@ git submodule init
 git submodule update
 popd >/dev/null
 
+# Get the path to dotfiles from $HOME
+RELADIR="$( realpath --relative-to "$HOME" "$DIR" )"
+cd "$HOME"
+
 ENTITIES=(.vim .vimrc .gitignore_global .gitconfig .bashrc .inputrc .ackrc)
 for entity in ${ENTITIES[@]}; do
   if [ ! -L "$HOME/$entity" ]; then
-    ln -s "$DIR/$entity" "$HOME/$entity"
+    ln -r -s "$RELADIR/$entity" "$HOME/$entity"
   fi
 done
+exit
 
 mkdir -p "$HOME/.config/bspwm/"
-ln -s "$DIR/bspwmrc" "$HOME/.config/bspwm/bspwmrc"
+ln -r -s "$RELADIR/bspwmrc" "$HOME/.config/bspwm/bspwmrc"
 mkdir -p "$HOME/.config/sxhkd/"
-ln -s "$DIR/sxhkdrc" "$HOME/.config/sxhkd/sxhkdrc"
+ln -r -s "$RELADIR/sxhkdrc" "$HOME/.config/sxhkd/sxhkdrc"
 
 mkdir -p "$HOME/.config/fish/"
-ln -s "$DIR/fish/config.fish" "$HOME/.config/fish/config.fish"
+ln -r -s "$RELADIR/fish/config.fish" "$HOME/.config/fish/config.fish"
 
 if [ ! -L "$HOME/.vim/spell" ]; then
   ln -s "$HOME/Dropbox/vim-spell" "$HOME/.vim/spell"
