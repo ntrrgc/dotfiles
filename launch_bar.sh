8a641c043585caa261918a7d74e8ea12902fb84f
Juan Luis Boya García <ntrrgc@gmail.com>
2016-01-24 05:38:56 +0100

    Awesome icons

--- launch_bar.sh
+++ launch_bar.sh
@@ -3,7 +3,7 @@ PANEL_HEIGHT=22
 bspc config top_padding $PANEL_HEIGHT
 
 ~/Programas/bar/lemonbar \
-  -f "Roboto-10,Koruri-10,sm4tik" \
+  -f "Roboto-10,Koruri-10,FontAwesome,sm4tik" \
   -B '#000000' \
   -g 3840x$PANEL_HEIGHT \
   -u 2 -o -3 | bash
