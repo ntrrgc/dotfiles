b950d48f09837f87481e977475c902b3d17e5b8f
Juan Luis Boya García <ntrrgc@gmail.com>
2017-02-12 14:00:32 +0100

    Reorder fonts in order to work with updated DejaVu Sans
    Prioritize ntrrgcbarfont and FontAwesome in order to keep showing correct icons after update in DejaVu Sans which added many glyphs to the BMP Private Use Area.

--- launch_bar.sh
+++ launch_bar.sh
@@ -3,9 +3,8 @@ PANEL_HEIGHT=22
 
 bspc config top_padding $PANEL_HEIGHT
 
-#-f "Roboto-10" -f "Koruri-10" -f "FontAwesome" -f "sm4tik" -f "ntrrgcbarfont" \
 ~/Programas/bar/lemonbar \
-  -f "Roboto-10,Koruri-10,FontAwesome,sm4tik,ntrrgcbarfont" \
+  -f "ntrrgcbarfont,FontAwesome,Roboto-10,Koruri-10,sm4tik" \
   -B '#000000' \
   -g 3840x$PANEL_HEIGHT \
   -u 2 -o -3 | bash
