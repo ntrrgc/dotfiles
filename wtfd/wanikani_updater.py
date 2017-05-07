bf3b67f89ec2319f2368b016d6ddd4e2aad6fbaa
Alicia Boya García <ntrrgc@gmail.com>
2017-05-07 05:13:24 +0200

    Cache wanikani responses so I can restart how much as I want

--- wtfd/wanikani_updater.py
+++ wtfd/wanikani_updater.py
@@ -1,9 +1,10 @@
 import json
 import os
+from functools import partial
 from time import time
 
 from tornado.httpclient import AsyncHTTPClient
-from tornado.ioloop import PeriodicCallback
+from tornado.ioloop import PeriodicCallback, IOLoop
 
 from wtfd.bar_singleton import bar
 from wtfd.debug import debug
@@ -18,17 +19,44 @@ http_client = AsyncHTTPClient(defaults=dict(user_agent="Wanikani reviews widget"
 
 switch_latest_study_queue = SwitchLatestOperator()
 
+def read_cached_wanikani_queue():
+    try:
+        with open("/tmp/.wanikani-update", "r") as f:
+            data = json.load(f)
+            seconds_old = time() - data["time"]
+            if seconds_old < 100:
+                return data["response"]
+    except OSError:
+        return None
+
+def write_cached_wanikani_queue(json_response):
+    with open("/tmp/.wanikani-update", "w") as f:
+        debug(repr(json_response))
+        json.dump({
+            "time": time(),
+            "response": json_response,
+        }, f)
 
 def request_update_wanikani():
-    http_client.fetch("https://www.wanikani.com/api/user/{api_key}/study-queue".format(api_key=api_key),
-                      switch_latest_study_queue.wrap(response_update_wanikani))
+    cached_response = read_cached_wanikani_queue()
+    if cached_response:
+        IOLoop.instance().call_later(0, partial(switch_latest_study_queue.wrap(response_update_wanikani),
+                                                cached_response)),
+    else:
+        http_client.fetch("https://www.wanikani.com/api/user/{api_key}/study-queue".format(api_key=api_key),
+                          switch_latest_study_queue.wrap(response_save_cache_update_wanikani))
 
-def response_update_wanikani(response):
+def response_save_cache_update_wanikani(response):
     if response.error:
         debug("Error requesting Wanikani study queue: %s" % response.error)
         return
 
-    data = json.loads(response.body)["requested_information"]
+    json_response = json.loads(response.body)
+    write_cached_wanikani_queue(json_response)
+    response_update_wanikani(json_response)
+
+def response_update_wanikani(json_response):
+    data = json_response["requested_information"]
 
     # Note: this requires a clock roughly synchronized with NTP to work correctly
     seconds_next_review = max(0, data["next_review_date"] - time())
