23af860b9ae5b53cc216d0161f8dea4bacc0f8b8
Alicia Boya García <ntrrgc@gmail.com>
2024-07-19 12:31:08 +0200

    ipython-startup.py: subprocess, shlex

--- ipython-startup.py
+++ ipython-startup.py
@@ -2,6 +2,7 @@ import os, sys, json, shutil, re, tty, termios
 import time, pprint, datetime
 from pathlib import Path, PurePath, PurePosixPath, PureWindowsPath
 from enum import Enum, StrEnum
+import subprocess, shlex
 
 try:
     import numpy as np
