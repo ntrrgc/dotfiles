fb2ae5e64e83908ecfb4f5ae859a78a3635a2ddb
Alicia Boya García <ntrrgc@gmail.com>
2024-09-12 22:17:27 +0200

    ipython-startup.py: Import even more basic stuff

--- ipython-startup.py
+++ ipython-startup.py
@@ -1,9 +1,15 @@
+from __future__ import annotations
+from typing import *
+from abc import ABC, ABCMeta
+from dataclasses import dataclass, field
 import os, sys, json, shutil, re, tty, termios
 import time, pprint, datetime
 from pathlib import Path, PurePath, PurePosixPath, PureWindowsPath
-from enum import Enum, StrEnum
+from enum import Enum, StrEnum, IntEnum
 import subprocess, shlex
 from math import *
+from itertools import *
+from functools import *
 
 try:
     import numpy as np
